3150a82e7066dea634701653bbc83b55f42c1f40
luteh <luthfanmaftuh@gmail.com>
2019-07-09 09:29:47 +0700

    Add dbhelper function on repository

--- app/src/main/java/com/luteh/madesubmission1/data/MyRepository.kt
+++ app/src/main/java/com/luteh/madesubmission1/data/MyRepository.kt
@@ -2,6 +2,10 @@ package com.luteh.madesubmission1.data
 
 import com.luteh.madesubmission1.data.model.api.movie.MovieResponse
 import com.luteh.madesubmission1.data.model.api.tvshow.TvShowResponse
+import com.luteh.madesubmission1.data.model.db.MovieDb
+import com.luteh.madesubmission1.data.model.db.TvShowDb
+import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Single
 
 /**
@@ -12,4 +16,12 @@ interface MyRepository {
 
     fun getMovieData(language: String): Single<MovieResponse>
     fun getTvShowData(language: String): Single<TvShowResponse>
+
+    fun saveMovie(movieDb: MovieDb): Completable
+    fun loadAllMovies(): Flowable<List<MovieDb>>
+    fun deleteMovieById(movieId: String)
+
+    fun saveTvShow(tvShowDb: TvShowDb): Completable
+    fun loadAllTvShows(): Flowable<List<TvShowDb>>
+    fun deleteTvShowById(tvShowId: String)
 }
--- app/src/main/java/com/luteh/madesubmission1/data/MyRepositoryImpl.kt
+++ app/src/main/java/com/luteh/madesubmission1/data/MyRepositoryImpl.kt
@@ -3,13 +3,35 @@ package com.luteh.madesubmission1.data
 import com.luteh.madesubmission1.data.local.MyDbHelper
 import com.luteh.madesubmission1.data.model.api.movie.MovieResponse
 import com.luteh.madesubmission1.data.model.api.tvshow.TvShowResponse
+import com.luteh.madesubmission1.data.model.db.MovieDb
+import com.luteh.madesubmission1.data.model.db.TvShowDb
 import com.luteh.madesubmission1.data.remote.ApiServiceInterface
+import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Single
 
 class MyRepositoryImpl(
     private val apiServiceInterface: ApiServiceInterface,
     private val myDbHelper: MyDbHelper
 ) : MyRepository {
+
+    override fun saveMovie(movieDb: MovieDb): Completable =
+        myDbHelper.saveMovie(movieDb)
+
+    override fun loadAllMovies(): Flowable<List<MovieDb>> =
+        myDbHelper.loadAllMovies()
+
+    override fun deleteMovieById(movieId: String) =
+        myDbHelper.deleteMovieById(movieId)
+
+    override fun saveTvShow(tvShowDb: TvShowDb): Completable =
+        myDbHelper.saveTvShow(tvShowDb)
+
+    override fun loadAllTvShows(): Flowable<List<TvShowDb>> =
+        myDbHelper.loadAllTvShows()
+
+    override fun deleteTvShowById(tvShowId: String) =
+        myDbHelper.deleteTvShowById(tvShowId)
 
     override fun getMovieData(language: String): Single<MovieResponse> =
         apiServiceInterface.getMovieData(language)
