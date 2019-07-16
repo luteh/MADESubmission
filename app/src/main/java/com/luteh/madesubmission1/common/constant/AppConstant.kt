28c788b9ba6199dbb5483f13ea99d71f437ce1f9
luteh <luthfanmaftuh@gmail.com>
2019-07-16 10:23:26 +0700

    Change Api Key declaration place onto buildconfig

--- app/src/main/java/com/luteh/madesubmission1/common/constant/AppConstant.kt
+++ app/src/main/java/com/luteh/madesubmission1/common/constant/AppConstant.kt
@@ -1,5 +1,7 @@
 package com.luteh.madesubmission1.common.constant
 
+import com.luteh.madesubmission1.BuildConfig
+
 /**
  * Created by Luthfan Maftuh on 6/24/2019.
  * Email luthfanmaftuh@gmail.com
@@ -8,7 +10,7 @@ object AppConstant {
 
     const val BASE_URL = "https://api.themoviedb.org/"
     const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w185/"
-    const val API_KEY = "e7e51ae93f8d998945ad19fb49e1bb5a"
+    const val API_KEY = BuildConfig.TMDB_API_KEY
 
     const val KEY_BUNDLE_HOME_DATA = "KEY_BUNDLE_HOME_DATA"
 
