18b35739a6e7ade37b0f2680373d03d265ec4009
luteh <luthfanmaftuh@gmail.com>
2019-08-25 00:15:58 +0700

    Add library

---
+++ favoritemovieapp/src/main/java/com/luteh/favoritemovieapp/common/AppConstant.kt
@@ -0,0 +1,11 @@
+package com.luteh.favoritemovieapp.common
+
+/**
+ * Created by Luthfan Maftuh on 8/25/2019.
+ * Email luthfanmaftuh@gmail.com
+ */
+object AppConstant {
+
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w185/"
+
+}
