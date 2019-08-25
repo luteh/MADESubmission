69ec6ed23996c8e51c8c9b9e33ae86c5beab6295
luteh <luthfanmaftuh@gmail.com>
2019-08-25 08:36:11 +0700

    Implement adapter to view

--- favoritemovieapp/src/main/java/com/luteh/favoritemovieapp/MainActivity.kt
+++ favoritemovieapp/src/main/java/com/luteh/favoritemovieapp/MainActivity.kt
@@ -4,10 +4,12 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.luteh.favoritemovieapp.data.MovieData
+import com.luteh.favoritemovieapp.ui.MainAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG = "MainActivity"
 
     /** The authority of this content provider.  */
     val AUTHORITY = "com.luteh.madesubmission1"
@@ -19,24 +21,41 @@ class MainActivity : AppCompatActivity() {
         "content://$AUTHORITY/$TABLE"
     )
 
+    private val mainAdapter = MainAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onInit()
+    }
+
+    private fun onInit() {
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        rv_main.apply {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = mainAdapter
+        }
     }
 
     override fun onResume() {
         super.onResume()
         val cursor = contentResolver.query(URI_MOVIE, null, null, null, null)
+        val movieDatas: MutableList<MovieData> = mutableListOf()
 
         if (cursor != null) {
             while (cursor.moveToNext()) {
-                val id = cursor.getString(cursor.getColumnIndex("id"))
-                val title = cursor.getString(cursor.getColumnIndex("title"))
-
-                Log.d(TAG, "onCreate: $id $title")
+                movieDatas.add(MovieData.fromCursor(cursor))
             }
 
             cursor.close()
         }
+
+        mainAdapter.setDataSources(movieDatas)
     }
 }
