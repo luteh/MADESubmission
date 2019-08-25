a3efef348c77f00f108f838cdfb772adb6d89b30
luteh <luthfanmaftuh@gmail.com>
2019-08-25 11:22:39 +0700

    Add no data item layout

--- favoritemovieapp/src/main/java/com/luteh/favoritemovieapp/ui/MainActivity.kt
+++ favoritemovieapp/src/main/java/com/luteh/favoritemovieapp/ui/MainActivity.kt
@@ -1,15 +1,21 @@
-package com.luteh.favoritemovieapp
+package com.luteh.favoritemovieapp.ui
 
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import com.luteh.favoritemovieapp.R
 import com.luteh.favoritemovieapp.data.MovieData
-import com.luteh.favoritemovieapp.ui.MainAdapter
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     /** The authority of this content provider.  */
     val AUTHORITY = "com.luteh.madesubmission1"
@@ -21,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         "content://$AUTHORITY/$TABLE"
     )
 
+    private val compositeDisposable = CompositeDisposable()
+
     private val mainAdapter = MainAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +39,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onInit() {
-
         setupRecyclerView()
     }
 
@@ -45,6 +52,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+
+        compositeDisposable.add(
+            Single.fromCallable { getMovieData() }
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ movieDatas ->
+                    mainAdapter.setDataSources(movieDatas)
+                }, { throwable ->
+                    Log.e(TAG, "onResume: $throwable")
+                })
+        )
+
+    }
+
+    private fun getMovieData(): List<MovieData> {
         val cursor = contentResolver.query(URI_MOVIE, null, null, null, null)
         val movieDatas: MutableList<MovieData> = mutableListOf()
 
@@ -56,6 +78,11 @@ class MainActivity : AppCompatActivity() {
             cursor.close()
         }
 
-        mainAdapter.setDataSources(movieDatas)
+        return movieDatas
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 }
